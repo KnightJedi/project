ef01ebab5f7e26acc724fef60f6552f4c54d1fa1
KnightJedi <sirmaster5mail@gmail.com>
2014-03-30 20:48:12 +1100

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 4.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<meta http-equiv="x-ua-compatible" content="IE=edge"/>
+
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1251" />
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head>
+	<title>Корзина</title>
+	<link rel="stylesheet"  type="text/css" href="jquery-ui-1.8.23.custom.css" />
+	<link rel="stylesheet"  type="text/css" href="style_css.css" />
+	<link href='http://fonts.googleapis.com/css?family=Forum&subset=cyrillic-ext,latin' rel='stylesheet' type='text/css'>
+		<script type="text/javascript" src="js/modernizr.custom.79639.js"></script> 
+  	<meta name="viewport" content="initial-scale=1, maximum-scale=1" /> 
+</head>
+<body>
+	<div class="page">
+		<div class="wrap">
+			<div id="content">
+				<div class="inner">
+					<div class="poisk">
+						<div class="vibortext">Добро пожаловать!</div>
+						<img class="bg" src="bg.png"	alt=""> 
+						<form class="inprod" action="stage0.php">
+							<input class="naiti" type="submit" name="add" value="Закупимся?"/>
+						</form>	
+					</div><!--End Poisk-->
+				</div><!--End Inner-->
+			</div><!--End Content-->
+		</div><!--End Wrap-->
+	</div><!--End Page-->
+</body>
+</html>
